4fa2c5351eda9f813aa10d43e8d0c132b4870086
Anil kumar rajput <rajputanilkumar@gmail.com>
2025-12-28 15:58:31 +0530

    Update test_logconv.py

--- tests/test_logconv.py
+++ tests/test_logconv.py
@@ -11,8 +11,8 @@ async def example_test(dut):
     pass
 def test_logconv_runner():
     sim = os.getenv("SIM", "icarus")
-    proj_path = Path(__file__).resolve().parent
-    sources = [proj_path / "sources/logconv.v"]
+    proj_path = Path(__file__).resolve().parent.parent
+    sources = [proj_path /"sources/logconv.v"]
     runner = get_runner(sim)
     runner.build(
         sources=sources,
